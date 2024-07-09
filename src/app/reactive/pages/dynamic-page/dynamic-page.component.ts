d72532e7e3da90b0bd4179f604d9c1cbdc6043de
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-09 14:33:14 -0600

    feat: Formularios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+
+  public myForm:FormGroup = this.fb.group({
+    name         : ['', [Validators.required, Validators.minLength(3)] ],
+    favoriteGames: this.fb.array([
+      [ 'Metal Gear', Validators.required ],
+      [ 'Death Stranding', Validators.required ],
+    ])
+  });
+
+  constructor( private fb:FormBuilder ){}
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit():void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log( this.myForm.value );
+    this.myForm.reset();
+  }
 
 }
