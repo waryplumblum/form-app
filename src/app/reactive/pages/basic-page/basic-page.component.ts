597be67094cfe8dd636ff96c9e9b3318c5bd8228
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-09 12:27:53 -0600

    feat: Métodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -32,8 +32,29 @@ export class BasicPageComponent implements OnInit {
     this.myForm.reset( rtx5090 );
   }
 
+  isValidField( field:string ):boolean|null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field:string):string|null{
+    if( !this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for(const key of Object.keys(errors)){
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracters.`;
+      }
+    }
+    return null;
+  }
+
   onSave():void{
-    if( this.myForm.invalid ) return;
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log( this.myForm.value );
     this.myForm.reset({ price:0, inStorage:0 })
   }
