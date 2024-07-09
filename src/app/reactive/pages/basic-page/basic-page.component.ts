39baea700bb39c47eed06fce4a939f32a55099c0
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-08 21:27:00 -0600

    feat: new components

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+}
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dynamic-page.component.html',
+  styles: ``
+})
+export class DynamicPageComponent {
+
+}
