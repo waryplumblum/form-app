3ac5f7622aef38809237897814ce8cb9e5469ef3
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-09 15:02:07 -0600

    feat: eliminar elementos de un formarray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -43,6 +43,10 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onDeleteFavorite ( index:number ):void {
+    this.favoriteGames.removeAt(index);
+  }
+
   onSubmit():void{
     if(this.myForm.invalid){
       this.myForm.markAllAsTouched();
