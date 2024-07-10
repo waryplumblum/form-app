17cd67b5bd92bbb2176b928870422ff340febf65
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-10 12:30:22 -0600

    feat: Register form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm:FormGroup = this.fb.group({
+    name:      ['', [Validators.required]],
+    email:     ['', [Validators.required]],
+    username:  ['', [Validators.required]],
+    password:  ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  })
+
+  constructor( private fb:FormBuilder ){}
+
+  isValiedField( field:string ){
+    // TODO: Obtener validación desde un servicio.
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 
 }
