f9d96fc4639b03e39f003811898e61e3210797a1
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-09 22:23:36 -0600

    feat: switches pages

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm:FormGroup = this.fb.group({
+    gender: ['M',Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  public person ={
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  constructor( private fb:FormBuilder ){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.myForm.reset(this.person);
+  }
+
+  isValidField( field:string ):boolean|null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+
+  }
 
 }
