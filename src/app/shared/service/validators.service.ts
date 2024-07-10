f922c2be61104bf4b4d50875cf9908b535598336
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-10 17:52:00 -0600

    feat: Verify that 2 fields are the same

--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
 export class ValidatorsService {
@@ -22,6 +22,19 @@ export class ValidatorsService {
 
   isValidField( form:FormGroup,field:string ):boolean|null{
     return form.controls[field].errors && form.controls[field].touched;
+  }
+
+  public isFieldOneEqualFieldTwo( field1:string,field2:string){
+    return ( formGroup:AbstractControl ):ValidationErrors | null => {
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+      if( fieldValue1 !== fieldValue2 ){
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true }
+      }
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
   }
 
 }
--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -33,8 +33,6 @@ export class EmailValidatorService implements AsyncValidator{
       delay(3000)
     );
     return httpCallObservable;
-
-
   }
 
 }
